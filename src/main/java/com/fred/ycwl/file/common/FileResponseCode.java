e23ebea303028e62bb71f3d4f202a51d6369d083
81673211 <81673211@qq.com>
2019-04-23 11:24:23 +0800

    exception and error handle

--- src/main/java/com/fred/ycwl/file/common/FileResponseCode.java
+++ src/main/java/com/fred/ycwl/file/common/FileResponseCode.java
@@ -11,17 +11,18 @@ import com.fred.ycwl.common.web.ResponseCode;
  */
 public class FileResponseCode extends ResponseCode {
 
-    public static final ResponseCode MULTIPART_FILE_NULL = new FileResponseCode("01001", "上传的文件为空");
-    public static final ResponseCode MULTIPART_FILE_NAME_BLANK = new FileResponseCode("01002", "上传的文件名为空");
+    public static final ResponseCode MULTIPART_FILE_NULL = new ResponseCode("01001", "上传的文件为空");
+    public static final ResponseCode MULTIPART_FILE_NAME_BLANK = new ResponseCode("01002", "上传的文件名为空");
     public static final ResponseCode MULTIPART_FILE_NAME_ILLEGAL =
-            new FileResponseCode("01003", "上传的文件名不合规，文件名只能包含中文、字母和数字");
+            new ResponseCode("01003", "上传的文件名不合规，文件名只能包含中文、字母和数字");
     public static final ResponseCode MULTIPART_FILE_EXTENSION_ILLEGAL =
-            new FileResponseCode("01004", "上传的文件扩展名不合规，扩展名只能是JPG、JPEG、PNG、MP4");
+            new ResponseCode("01004", "上传的文件扩展名不合规，扩展名只能是JPG、JPEG、PNG、MP4");
     public static final ResponseCode UPLOAD_FILE_ERROR =
-            new FileResponseCode("01005", "上传文件失败，请联系IT");
+            new ResponseCode("01005", "上传文件失败，请联系IT");
 
+    private static final long serialVersionUID = 7833635874982917579L;
 
-    private FileResponseCode(String errorCode, String message) {
+    public FileResponseCode(String errorCode, String message) {
         super(errorCode, message);
     }
 }
